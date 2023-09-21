564782e742ad9dec1228f31852891960fc1428ac
Sonia Uwase <usonia@mit.edu>
2023-09-20 23:27:15 -0400

    login bug fix

--- src/concepts/websession.ts
+++ src/concepts/websession.ts
@@ -1,9 +1,9 @@
 // This is (some of) the code for the WebSession concept which was introduced in lecture on 9/18.
-// We're storing the user (in the form of the username string for now) when the user logs in, and we 
+// We're storing the user (in the form of the username string for now) when the user logs in, and we
 // reset the session's user when the user logs out.
 
 import { SessionData } from "express-session";
-import { UnauthenticatedError } from "./errors";
+import { NotAllowedError, UnauthenticatedError } from "./errors";
 
 export type WebSessionDoc = SessionData;
 
@@ -28,6 +28,7 @@ export default class WebSessionConcept {
     // Hint: Take a look at how the "end" function makes sure the user is logged in. Keep in mind that a
     // synchronization like starting a session should just consist of a series of actions that may throw
     // exceptions and should not have its own control flow.
+    this.isNotLoggedIn(session);
     session.user = username;
   }
 
@@ -40,6 +41,12 @@ export default class WebSessionConcept {
     // We make sure the user is logged in before allowing the end action.
     this.isActive(session);
     session.user = undefined;
+  }
+
+  isNotLoggedIn(session: WebSessionDoc) {
+    if (session.user !== undefined) {
+      throw new NotAllowedError("Already logged in!");
+    }
   }
 
   // Helper functions
